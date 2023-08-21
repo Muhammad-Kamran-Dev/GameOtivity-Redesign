44939060888ec1fd26633679e67195e2fbed989c
Muhammad Kamran <kamran8915uop@gmail.com>
2023-08-21 18:50:32 +0500

    lazy loading remove from feature section

--- src/app/components/Features/FeatureCard.tsx
+++ src/app/components/Features/FeatureCard.tsx
@@ -10,7 +10,7 @@ const FeatureCard: React.FC<FeatureCardType> = ({ img, title, text }) => {
   return (
     <article className="flex flex-col gap-5 max-w-[400px] justify-center items-center ">
       <div className="w-72 h-72  relative">
-        <Image src={img} fill className="object-contain" alt={title} />
+        <Image src={img} priority fill className="object-contain" alt={title} />
       </div>
       <h2 className="text-2xl font-bold">{title}</h2>
       <p className="text-center">{text}</p>
